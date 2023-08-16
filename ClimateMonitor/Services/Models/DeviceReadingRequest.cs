bce00b491afaf588381095d7c9cffcc3efe716f4
GuilhermeSilva83 <guilhesas@hotmail.com>
2023-08-16 18:11:26 -0300

    my first commit

---
+++ ClimateMonitor/Services/Models/DeviceReadingRequest.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ClimateMonitor.Services.Models;
+
+public class DeviceReadingRequest
+{
+
+    [RegularExpression(@"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9azA-Z-]+)*))?$",
+             
+        ErrorMessage = "The firmware value does not match semantic versioning format.")]
+    [Required]
+    public string FirmwareVersion { get; set; } = string.Empty;
+
+    [Required]
+    public decimal Temperature { get; set; }
+
+    [Required]
+    public decimal Humidity { get; set; }
+}
